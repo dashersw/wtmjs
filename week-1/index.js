819810c7cb318b2e9df157f50eb51e4607c25296
Armagan Amcalar <armagan@amcalar.com>
2017-10-05 11:07:17 +0200

    Add simple module example for week 1

---
+++ week-1/index.js
@@ -0,0 +1,6 @@
+import Person from './person.js';
+
+const person = new Person('Armagan Amcalar');
+person.interests.push('JavaScript', 'Photography', 'Speaking');
+
+console.log(person);
---
+++ week-1/person.js
@@ -0,0 +1,6 @@
+export default class Person {
+    constructor(name) {
+        this.name = name;
+        this.interests = [];
+    }
+}
